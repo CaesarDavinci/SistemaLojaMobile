3eeebc6458c62bdf3c80fb3d21b45613df47c651
CaesarDavinci <marioalves.eng@gmail.com>
2020-12-29 20:55:58 -0300

    Projeto Criado

---
+++ src/test/java/com/axis/SistemaLojaMobile/EventosApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axis.SistemaLojaMobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
