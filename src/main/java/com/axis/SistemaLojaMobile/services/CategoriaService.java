4e8d6778e3cd07241e92e3f7fdbc7dd933d1549a
CaesarDavinci <marioalves.eng@gmail.com>
2020-12-30 00:19:37 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/axis/SistemaLojaMobile/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.axis.SistemaLojaMobile.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.axis.SistemaLojaMobile.Domain.Categoria;
+import com.axis.SistemaLojaMobile.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
