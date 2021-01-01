66a7a9b3c76403e299605990dfc96bc0f499bd3e
CaesarDavinci <marioalves.eng@gmail.com>
2021-01-01 11:24:21 -0300

    Estado e cidade

--- src/main/java/com/axis/SistemaLojaMobile/EventosAppApplication.java
+++ src/main/java/com/axis/SistemaLojaMobile/EventosAppApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.axis.SistemaLojaMobile.Domain.Categoria;
+import com.axis.SistemaLojaMobile.Domain.Cidade;
+import com.axis.SistemaLojaMobile.Domain.Estado;
 import com.axis.SistemaLojaMobile.Domain.Produto;
 import com.axis.SistemaLojaMobile.repositories.CategoriaRepository;
+import com.axis.SistemaLojaMobile.repositories.CidadeRepository;
+import com.axis.SistemaLojaMobile.repositories.EstadoRepository;
 import com.axis.SistemaLojaMobile.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,14 @@ public class EventosAppApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository ;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventosAppApplication.class, args);
@@ -44,6 +56,20 @@ public class EventosAppApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
---
+++ src/main/java/com/axis/SistemaLojaMobile/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.axis.SistemaLojaMobile.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.axis.SistemaLojaMobile.Domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+	
+}
